9d0208ab2102a481438d20f6edacf74dce0dd2eb
cengiztuglu <cngztuglu@gmail.com>
2022-10-25 13:41:28 +0300

    Add classes

---
+++ TatilSeyahat/Models/Classes/Blog.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TatilSeyahat.Models.Classes
+{
+    public class Blog
+    {
+        public int ID { get; set; }
+        public string Baslik { get; set; }
+        public DateTime Tarih { get; set; }
+
+        public string Aciklama { get; set; }
+    }
+}
