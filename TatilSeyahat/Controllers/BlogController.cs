1276fd73ebf59e1cfbe50e19626db8a1dea982ab
cengiztuglu <cngztuglu@gmail.com>
2022-10-29 02:40:43 +0300

    Blog page

---
+++ TatilSeyahat/Controllers/BlogController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TatilSeyahat.Controllers;
+using TatilSeyahat.Models.Classes;
+
+namespace TatilSeyahat.Controllers
+{
+    public class BlogController : Controller
+    {
+        // GET: Blog
+        public ActionResult Index()
+        {
+            Context c = new Context();
+            var bloglar = c.Blogs.ToList();
+            
+            return View(bloglar);
+        }
+    }
+}
