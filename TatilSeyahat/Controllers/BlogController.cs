ebbe2a5f3e214c1ddae707cce94931838fa14c83
cengiztuglu <cngztuglu@gmail.com>
2022-10-29 05:09:54 +0300

    blog comment

--- TatilSeyahat/Controllers/BlogController.cs
+++ TatilSeyahat/Controllers/BlogController.cs
@@ -11,12 +11,21 @@ namespace TatilSeyahat.Controllers
     public class BlogController : Controller
     {
         // GET: Blog
+        Context c = new Context();
         public ActionResult Index()
         {
-            Context c = new Context();
+            
             var bloglar = c.Blogs.ToList();
             
             return View(bloglar);
+        }
+        BlogYorum by = new BlogYorum();
+        public   ActionResult BlogDetay(int id)
+        {
+            by.Deger1 = c.Blogs.Where(x => x.ID == id).ToList();
+            by.Deger2 = c.Yorumlars.Where(x => x.Blogid == id).ToList();
+            return View(by);
+
         }
     }
 }
---
+++ TatilSeyahat/Models/Classes/BlogYorum.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TatilSeyahat.Models.Classes
+{
+    public class BlogYorum
+    {
+        public IEnumerable<Blog> Deger1 { get; set; }
+        public IEnumerable<Yorumlar> Deger2  {get; set;}
+    }
+}
